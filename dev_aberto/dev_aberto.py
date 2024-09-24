c80fea3d5d402d9bce449f0b3aa2f6be29e6ffd1
AndreCorreaSantos <acorreasantos2002@gmail.com>
2024-09-23 22:33:25 -0300

    adding translation

--- dev_aberto/dev_aberto.py
+++ dev_aberto/dev_aberto.py
@@ -1,8 +1,28 @@
 import requests
-
+import gettext
+import os
+import locale
+from datetime import datetime
+from dateutil import parser
 
 def hello():
+    
+    lang = os.environ.get('LANGUAGE', 'en_US').split(':')[0]
+    locale.setlocale(locale.LC_ALL, lang)
+
+    locale_path = os.path.join(os.path.dirname(__file__), 'locale')
+    translation = gettext.translation('dev_aberto', localedir=locale_path, languages=[lang], fallback=True)
+    translation.install()
+    _ = translation.gettext
+
     c = requests.get('https://api.github.com/repos/insper/dev-aberto/commits')
     info = c.json()
     commit_info = info[0]['commit']['author']
-    return commit_info['date'], commit_info['name']
+    date = commit_info['date']
+    name = commit_info['name']
+
+
+    date_parsed = parser.isoparse(date)
+    date_formatted = date_parsed.strftime('%c')
+
+    return date_formatted, name
--- scripts/hello.py
+++ scripts/hello.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 from dev_aberto import hello
+import gettext
+import os
+import locale
 
 if __name__ == '__main__':
+
+    lang = os.environ.get('LANGUAGE', 'en_US').split(':')[0]
+    print(lang)
+    locale.setlocale(locale.LC_ALL, lang)
+
+    locale_path = os.path.join(os.path.dirname(__file__), 'locale')
+    translation = gettext.translation('dev_aberto', localedir=locale_path, languages=[lang], fallback=True)
+    translation.install()
+    _ = translation.gettext
+
     date, name = hello()
-    print('Último commit feito em:', date, ' por', name)
+    print(_('Último commit feito em:'), date, _('por'), name)
